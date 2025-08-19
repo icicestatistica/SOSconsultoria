dc167b851314bc96f5519a1420c0ca7b5a68cbb7
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2025-08-19 10:29:16 -0300

    Update anovamista.R

--- R/anovamista.R
+++ R/anovamista.R
@@ -1,6 +1,12 @@
 anova_mista = function(resp,nomeresp,entre,nomeentre,intra,nomeintra,id){
   
-df = data.frame(resp,entre,intra,id) %>% na.omit()
+    df2 = data.frame(resp, entre, intra, id) %>% na.omit()
+    
+  df = df2 %>% 
+  pivot_wider(names_from = intra,values_from = resp) %>% 
+  na.omit() %>% 
+  pivot_longer(cols=-c(1,2)) %>% 
+  rename("intra"=3,"resp"=4)
 
 mod.ANOVA <- ez::ezANOVA(data = df,
                      dv = .(resp),
