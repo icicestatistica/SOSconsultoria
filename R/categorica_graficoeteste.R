c5aedf142ec6092d9c0c8c3d4b8b75d6c9df8e2d
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2026-01-16 15:26:37 -0300

    Update categorica_graficoeteste.R

--- R/categorica_graficoeteste.R
+++ R/categorica_graficoeteste.R
@@ -35,43 +35,154 @@ descri = paste(paste(res,collapse=", ")," e ",paste(names(tablevar)[length(table
     resultados=list("testes"=testes,"result"=d,"texto"=testectexto,"interp"=interpretacao,"tabela"=testectabela,"grafico"=graficoc)
   return(resultados)}
 
-grafico_categorica <- function(var,nome, niveis='auto', cor='cyan4', ordenar=T,virgula=F){
-  require(dplyr)
-  require(forcats)
-  var = unlist(var)
-  if (niveis[1]=='auto') niveis = names(table(var))
-  var = factor(var, levels=niveis)
-  niveisnovo=vetor_comsep_c(niveis,11)
-  levels(var)=niveisnovo
-  tab <- data.frame(table(var),perc=paste0(table(var),paste0(" (",100*round(prop.table(table(var)),3),"%)")),prop=paste0(table(var),paste0("\n  (",100*round(prop.table(table(var)),3),"%)")))
-  if(ordenar==T) {
-    if(length(niveis) > 2) {
-    result <- na.omit(tab) %>% mutate(var=fct_reorder(var, desc(Freq))) %>%
-  ggplot() + geom_bar(aes(x=var,y=Freq),fill=cor,stat="identity")  + 
-    ylim(0,max(table(var))*1.1)+theme_clean()  + ylab("") + xlab("") + ggtitle(paste0(vetor_comsep_c(nome,50)," (n=",length(na.omit(var)),")",collapse=""))+            geom_text(aes(x=var,y=Freq),label=ponto_para_virgula(tab$perc,virgula),vjust=-0.5) +
-        theme(
-        plot.background = element_rect(colour="white"),
-        axis.text.x=element_text(size=12),
-        plot.title = element_text(hjust = 0.5),
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank())} else
-              {result = ggplot(tab, aes(x="",y=Freq,fill=var)) +
-              geom_bar(stat="identity", width=1) +
-              coord_polar("y", start=0) + theme_void(base_size=12) +
-              labs(fill="",title=paste0(vetor_comsep_c(nome,50)," (n=",length(na.omit(var)),")")) +
-              theme(plot.title = element_text(hjust = 0.5, size = ceiling(12 * 1.1), face = "bold"),
-                       plot.subtitle = element_text(size = ceiling(12 * 1.05)),
-              plot.background = element_rect(colour="white")) + 
-              geom_text(aes(label = ponto_para_virgula(prop,virgula)), color = "white", position = position_stack(vjust = 0.5)) +
-              scale_fill_manual(labels = vetor_comsep_c(niveis,11),values=lighten(cor,seq(0,0.3,(0.3/(length(tab$var)-1)))))}}
-  if(ordenar==F) {
-    result <- ggplot(tab) + geom_bar(aes(x=var,y=Freq),fill=cor,stat="identity")  + ylim(0,max(table(var))*1.1)+theme_clean()  + ylab("") + xlab("") +
- ggtitle(paste0(vetor_comsep_c(nome,50)," (n=",length(na.omit(var)),")",collapse=""))+ geom_text(aes(x=var,y=Freq),label=ponto_para_virgula(tab$perc,virgula),vjust=-0.5) +
-    theme(
-        plot.background = element_rect(colour="white"),
-        axis.text.x=element_text(size=12),
-        plot.title = element_text(hjust = 0.5),
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank())}
-return(result)}
+grafico_categorica = function(var,nome,niveis = "auto",cor = "cyan4",ordenar = T,virgula = F) {
+    require(dplyr)
+    require(forcats)
+
+    var = unlist(var)
+
+    if (niveis[1] == "auto")
+        niveis = names(table(var))
+
+    var = factor(var, levels = niveis)
+    niveisnovo = vetor_comsep_c(niveis, 11)
+    levels(var) = niveisnovo
+
+    tab <- data.frame(
+        table(var),
+        perc = paste0(
+            table(var),
+            paste0(" (", 100 * round(prop.table(table(var)), 3), "%)")
+        ),
+        prop = paste0(
+            table(var),
+            paste0(
+                "\n  (",
+                100 * round(prop.table(table(var)), 3),
+                "%)"
+            )
+        )
+    )
+
+    if (ordenar == T) {
+        if (length(niveis) > 2) {
+            result <- na.omit(tab) %>%
+                mutate(
+                    var = fct_reorder(var, desc(Freq))
+                ) %>%
+                ggplot() +
+                geom_bar(
+                    aes(x = var, y = Freq),
+                    fill = cor,
+                    stat = "identity"
+                ) +
+                ylim(0, max(table(var)) * 1.1) +
+                theme_clean() +
+                ylab("") +
+                xlab("") +
+                ggtitle(
+                    paste0(
+                        vetor_comsep_c(nome, 50),
+                        " (n=",
+                        length(na.omit(var)),
+                        ")",
+                        collapse = ""
+                    )
+                ) +
+                geom_text(
+                    aes(x = var, y = Freq),
+                    label = ponto_para_virgula(tab$perc, virgula),
+                    vjust = -0.5
+                ) +
+                theme(
+                    plot.background = element_rect(colour = "white"),
+                    axis.text.x = element_text(size = 12),
+                    plot.title = element_text(hjust = 0.5),
+                    panel.grid.major = element_blank(),
+                    panel.grid.minor = element_blank()
+                )
+        } else {
+            result = ggplot(tab, aes(x = "", y = Freq, fill = var)) +
+                geom_bar(stat = "identity", width = 1) +
+                coord_polar("y", start = 0) +
+                theme_void(base_size = 12) +
+                labs(
+                    fill = "",
+                    title = paste0(
+                        vetor_comsep_c(nome, 50),
+                        " (n=",
+                        length(na.omit(var)),
+                        ")"
+                    )
+                ) +
+                theme(
+                    plot.title = element_text(
+                        hjust = 0.5,
+                        size = ceiling(12 * 1.1),
+                        face = "bold"
+                    ),
+                    plot.subtitle = element_text(
+                        size = ceiling(12 * 1.05)
+                    ),
+                    plot.background = element_rect(colour = "white")
+                ) +
+                geom_text(
+                    aes(
+                        label = ponto_para_virgula(prop, virgula)
+                    ),
+                    color = "white",
+                    position = position_stack(vjust = 0.5)
+                ) +
+                scale_fill_manual(
+                    labels = vetor_comsep_c(niveis, 11),
+                    values = lighten(
+                        cor,
+                        seq(
+                            0,
+                            0.3,
+                            (0.3 / (length(tab$var) - 1))
+                        )
+                    )
+                )
+        }
+    }
+
+    if (ordenar == F) {
+        result <- ggplot(tab) +
+            geom_bar(
+                aes(x = var, y = Freq),
+                fill = cor,
+                stat = "identity"
+            ) +
+            ylim(0, max(table(var)) * 1.1) +
+            theme_clean() +
+            ylab("") +
+            xlab("") +
+            ggtitle(
+                paste0(
+                    vetor_comsep_c(nome, 50),
+                    " (n=",
+                    length(na.omit(var)),
+                    ")",
+                    collapse = ""
+                )
+            ) +
+            geom_text(
+                aes(x = var, y = Freq),
+                label = ponto_para_virgula(tab$perc, virgula),
+                vjust = -0.5
+            ) +
+            theme(
+                plot.background = element_rect(fill = NA, colour = NA),
+                axis.text.x = element_text(size = 12),
+                plot.title = element_text(hjust = 0.5),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank()
+            )
+    }
+
+    return(result)
+}
 
 grafico_categorica_vert = function (lesoes, nome, niveis = "auto", cor = "cyan4", ordenar = T, 
     virgula = F) 
