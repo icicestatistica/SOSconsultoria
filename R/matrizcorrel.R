a1cc71f69a71c5b6816f9d449c03f86892e6bfd8
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2026-07-02 22:57:04 -0300

    Update matrizcorrel.R

--- R/matrizcorrel.R
+++ R/matrizcorrel.R
@@ -1,27 +1,110 @@
-matrizcorrel <- function(x,nomes,metodo){
+matrizcorrel = function (x, nomes=NULL, metodo="pearson",use="pairwise.complete.obs", margin=100, size=3) 
+{
+  library(corrplot)
+  if(is.null(nomes)) nomes = names(x) else names(x) = nomes
+    m = cor(x, method = metodo, use = use)
+    a = cor.mtest(x, method = metodo, use = use)
+    n = length(nomes)
+    sig = matrix(nrow = n, ncol = n)
+    for (i in 1:n) for (j in 1:n) if (i == j) 
+        sig[i, j] = "-"
+    else if (i < j) 
+        sig[i, j] = ""
+    else if (a$p[i, j] < 0.01) 
+        sig[i, j] = paste0(round(m[i, j], 3), "**")
+    else if (a$p[i, j] < 0.05) 
+        sig[i, j] = paste0(round(m[i, j], 3), "*")
+    else sig[i, j] = paste0(round(m[i, j], 3))
+    result = data.frame(nomes, sig)
+    names(result) = c("", nomes)
+    names(x) = nomes
 
-library(corrplot)
-m=cor(x, method=metodo,use="na.or.complete")
-a=cor.mtest(x,method=metodo,use="na.or.complete")
-n=length(nomes)
+if(is.null(nomes)) nomes <- names(x) else names(x) <- nomes
 
-sig=matrix(nrow=n,ncol=n)
+m <- cor(x, method = metodo, use = use)
+a <- cor.mtest(x, method = metodo, use = use)
 
-for (i in 1:n)
-  for (j in 1:n)
-    if(i==j) sig[i,j]="-" else
-      if (i<j) sig[i,j]="" else
-          if(a$p[i,j]<0.01) sig[i,j]=paste0(round(m[i,j],3),"**") else if(a$p[i,j]<0.05) sig[i,j]=paste0(round(m[i,j],3),"*") else sig[i,j]=paste0(round(m[i,j],3))
+n <- ncol(m)
 
-result=data.frame(nomes,sig)
-names(result)=c("",nomes)
-names(x)=nomes           
-grafico = corrplot(cor(x, method=metodo), type="lower")        
+dados <- expand.grid(
+  Var1 = rownames(m),
+  Var2 = colnames(m)
+)
 
-testes = data.frame(Nome1 = "", Nome2 = printvetor(nomes, aspas=F),tipo = "matcorrel", sig_ou_não = NA, resumo = NA, sup = NA)
-return(list("testes"=testes,"result"=result,"grafico"=grafico))}
+dados$r <- c(m)
+dados$p <- c(a$p)
+
+dados$i <- match(dados$Var1, rownames(m))
+dados$j <- match(dados$Var2, colnames(m))
+
+dados <- dados %>%
+  mutate(
+    label = case_when(
+      i == j ~ "-",
+      i < j ~ "",
+      p < 0.01 ~ sprintf("%.3f**", r),
+      p < 0.05 ~ sprintf("%.3f*", r),
+      TRUE ~ sprintf("%.3f", r)
+    )
+  )
+
+# Apenas abaixo da diagonal
+dados2 <- dados %>%
+  filter(i > j)
+
+labs <- data.frame(
+  x = 1:(n-1),
+  y = rev(2:n)-0.4,
+  label = head(nomes, -1)
+)
+
+grafico = ggplot(dados2,
+       aes(x = Var2,
+           y = factor(Var1, levels = rev(rownames(m))),
+           fill = r)) +
+
+  geom_tile(color = "white") +
+
+#  geom_text(aes(label = label), size = 4) +
+
+  scale_fill_gradient2(
+    low = "coral3",
+    mid = "gray",
+    high = "cyan4",
+    midpoint = 0,
+    limits = c(-1,1)
+  ) +
+
+  coord_equal(clip = "off") +
+
+  labs(
+    x = NULL,
+    y = NULL,
+    fill = NULL
+  ) +
+
+  theme_minimal() +
+
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    plot.margin = margin(t = margin),
+    legend.position='bottom',
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  ) +
+  geom_text(
+    data = labs,
+    aes(x = x, y = y, label = label),
+    angle = 45,
+    hjust = 0,
+    vjust = 0,
+    inherit.aes = FALSE,
+    size = size
+  )
         
-matcorrel_meta = function(){
-  texto="- **Matriz de correlação**:
-Uma matriz de correlação consiste em linhas e colunas que mostram as correlações entre duas variáveis de cada vez, sendo uma especificada na linha e uma na coluna, de modo que cada célula da tabela contém o coeficiente de correlação. É uma ferramenta poderosa para resumir um grande conjunto de dados e para identificar e visualizar padrões nos dados fornecidos. \n"
-  return(list("texto"=texto,"bib"=NULL))}
+    testes = data.frame(Nome1 = "", Nome2 = printvetor(nomes, 
+        aspas = F), tipo = "matcorrel", sig_ou_não = NA, resumo = NA, 
+        sup = NA)
+    return(list(testes = testes, result = result, grafico = grafico))
+}
